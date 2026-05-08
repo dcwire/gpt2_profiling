c6cc32aae5b294d4ed23b8c500bc3b0f18959e70
Anas <anasthegreat986@gmail.com>
2026-05-08 10:38:26 +0000

    Complete causal self attention

---
+++ src/model/CausalSelfAttention.py
@@ -0,0 +1,48 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        assert config.n_embd % config.n_head == 0
+
+        # one matrix, output key, query and value
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias) 
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd 
+
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+    
+    def forward(self, x):
+        B, T, C = x.size()
+
+        qkv = self.c_attn(x) 
+
+        q, k, v = qkv.split(C, dim=2)
+
+        # (B, nh, T, hs)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) 
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+
+        y = self.resid_dropout(self.c_proj(y))
+        return y
+
---
+++ src/model/config.py
@@ -0,0 +1,10 @@
+
+class GPTConfig:
+    def __init__(self, vocab_size=50257, block_size=1024, n_layer=12, n_head=12, n_embd=768, dropout=0.1, bias=True) -> None:
+        self.vocab_size = vocab_size
+        self.block_size = block_size
+        self.n_layer = n_layer
+        self.n_head = n_head
+        self.n_embd = n_embd
+        self.dropout = dropout
+        self.bias = bias
