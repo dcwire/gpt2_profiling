114c494b326f208b44341797b4809999f47c6848
Anas <anasthegreat986@gmail.com>
2026-05-15 18:39:51 +0000

    Verify training loop, modify batch size

--- src/trainer.py
+++ src/trainer.py
@@ -29,7 +29,7 @@ def train_gpt():
 
     enc = tiktoken.get_encoding("gpt2")
 
-    total_batch_size = 81920 # 2^19? Why? This is in number of tokens, TODO: different for shakespeare
+    total_batch_size = 8192 # 2^19? Why? This is in number of tokens, TODO: different for shakespeare
     B = 8 # Batch size
     T = 1024 # Sequence length / Context window
 
@@ -53,7 +53,8 @@ def train_gpt():
     max_lr = 6e-4 
     min_lr = max_lr * 0.1
     warmup_steps = 2
-    max_steps = 4 # Depends on the dataset + batch size... TODO
+    max_steps = 37 # Depends on the dataset + batch size... TODO
+    num_epochs = 10
 
     def get_lr(it):
 
@@ -72,45 +73,52 @@ def train_gpt():
 
     # TODO: Logging, optimizer
     optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device_type=device_type)
+    for epoch in range(num_epochs):
+        t0_epoch = time.time()
+        print(f"starting epoch {epoch}")
+        for step in range(max_steps):
+            t0 = time.time()
+            last_step = (step == max_steps - 1)
 
-    for step in range(max_steps):
-        t0 = time.time()
-        last_step = (step == max_steps - 1)
+            model.train()
+            optimizer.zero_grad()
+            loss_accum = 0.0
 
-        model.train()
-        optimizer.zero_grad()
-        loss_accum = 0.0
+            for micro_step in range(grad_accum_steps):
+                x, y = train_loader.next_batch()
+                x, y = x.to(device), y.to(device)
 
-        for micro_step in range(grad_accum_steps):
-            x, y = train_loader.next_batch()
-            x, y = x.to(device), y.to(device)
-
-            with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
-                logits, loss = model(x, y)
+                with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+                    logits, loss = model(x, y)
+                
+                loss = loss / grad_accum_steps
+                loss_accum += loss.detach()
+                loss.backward()
             
-            loss = loss / grad_accum_steps
-            loss_accum += loss.detach()
-            loss.backward()
-        
-        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
-        lr = get_lr(step)
+            lr = get_lr(step)
 
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
-        
-        optimizer.step()
-        if device_type == "cuda":
-            torch.cuda.synchronize() # Wait for gpu to finish work
-        
-        t1 = time.time()
-        dt = t1 - t0
-
-        tokens_processed = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
-        tokens_per_sec = tokens_processed / dt
-
-        if master_process:
-            print(f"step {step:5d} | loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec: .2f}")
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
             
+            optimizer.step()
+            if device_type == "cuda":
+                torch.cuda.synchronize() # Wait for gpu to finish work
+            
+            t1 = time.time()
+            dt = t1 - t0
+
+            tokens_processed = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
+            tokens_per_sec = tokens_processed / dt
+
+            if master_process:
+                print(f"step {step:5d} | loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec: .2f}")
+        
+        t1_epoch = time.time()
+        dt_epoch = t1_epoch - t0_epoch
+
+        print(f"finishing epoch {epoch} | time: {dt_epoch:.2f}s")
+                
 
 
